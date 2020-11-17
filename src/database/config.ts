0a0451b2f2a0c10874b7790d6a247b8ca2cb2312
Oscar Alonso <oscaralsa@unisabana.edu.co>
2020-11-17 16:31:52 -0500

    Heroku update

--- src/database/config.ts
+++ src/database/config.ts
@@ -9,11 +9,14 @@ const portNumber: number = + process.env.RDS_PORT!;
 
 const connection: Promise<any> = createConnection({
   type: "postgres",
-  host: process.env.RDS_HOSTNAME!,
-  port: portNumber!,
-  username: process.env.RDS_USERNAME!,
-  password: process.env.RDS_PASSWORD!,
-  database: process.env.RDS_DB_NAME!,
+  url: process.env.DATABASE_URL,
+  /* If you have a external database
+    host: process.env.RDS_HOSTNAME!,
+    port: portNumber!,
+    username: process.env.RDS_USERNAME!,
+    password: process.env.RDS_PASSWORD!,
+    database: process.env.RDS_DB_NAME!,
+  */
   entities,
   synchronize: true,
   logging: false
